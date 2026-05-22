f4a633ec7f17d9cd3e98f3e4de1634f4ec91f9f7
StepinIvan <ivan.stepin.99@mail.ru>
2026-05-22 11:45:04 +0700

    some fixes 2

--- src/main/java/ru/otus/java/basic/lesson1/MainApplication.java
+++ src/main/java/ru/otus/java/basic/lesson1/MainApplication.java
@@ -28,6 +28,6 @@ public class MainApplication {
             System.out.println(a + " / " + b + " = " + result);
         }
         System.out.println("1");
-        System.out.println("1");
+        System.out.println("2");
     }
 }
